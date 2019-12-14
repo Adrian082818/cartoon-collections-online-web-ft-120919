da54e1f16ee5455daf532bc230abd5ecd90ebee8
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-14 14:42:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,7 @@
 def roll_call_dwarves(array)
   index = 1 
-  
-  array.each_with_index{ |name, index| puts #{index} #{name} }
+  array.each_with_index{ |name, index| puts #{index} #{name}}
   index += 1 
-end
 end 
 
 def summon_captain_planet# code an argument here
