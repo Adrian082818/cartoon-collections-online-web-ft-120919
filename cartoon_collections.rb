dac4de28f6fb64e35c90998ce07d78660e68241f
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-14 15:11:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array)
 end 
 
 def summon_captain_planet(planeteer_calls)
-planeteer_calls.collect{|element| element. + "!"}
+planeteer_calls.collect{|element| element + "!"}
+element.titleize
 end
 
 def long_planeteer_calls(calls)
